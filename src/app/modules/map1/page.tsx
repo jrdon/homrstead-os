e49b822fed6c0af2aca5232a52c7a350b8c7b493
jrdon <40438100+jrdon@users.noreply.github.com>
2026-01-06 03:09:18 -0700

    Update page.tsx

--- src/app/modules/map1/page.tsx
+++ src/app/modules/map1/page.tsx
@@ -1,1 +1,41 @@
+Code
+Issues
+Pull requests
+Actions
+Projects
+Wiki
+homrstead-os/src/app
+/modules/
+jrdon
+jrdon
+Create map
+b4c3e27
+ · 
+10 minutes ago
+Name	Last commit message	Last commit date
+..
+apiary
+Initial Homestead OS frontend
+4 days ago
+irrigation
+Initial Homestead OS frontend
+4 days ago
+livestock
+Initial Homestead OS frontend
+4 days ago
+map1
+Create map
+10 minutes ago
+orchard
+Initial Homestead OS frontend
+4 days ago
+potatoes
+Initial Homestead OS frontend
+4 days ago
+weather
+Initial Homestead OS frontend
+4 days ago
+map
+Delete map file
+32 minutes ago
 
