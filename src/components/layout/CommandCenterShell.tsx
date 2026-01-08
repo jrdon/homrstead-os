5158516d3b3c74832f67c7d3ea96b8fb76bf1443
jrdon <40438100+jrdon@users.noreply.github.com>
2026-01-08 05:43:17 -0700

    Update CommandCenterShell.tsx

--- src/components/layout/CommandCenterShell.tsx
+++ src/components/layout/CommandCenterShell.tsx
@@ -5,13 +5,13 @@ import TopBar from "../navigation/TopBar";
 
 export default function CommandCenterShell({ children }: { children: React.ReactNode }) {
   return (
-    <div className="min-h-screen flex flex-row bg-brand-bg text-white">
+    <div className="h-screen flex flex-row bg-brand-bg text-white overflow-hidden">
       <Sidebar />
 
-      <div className="flex flex-col flex-1">
+      <div className="flex flex-col flex-1 overflow-hidden">
         <TopBar />
 
-        <main className="p-6 flex-1 overflow-auto">
+        <main className="flex-1 overflow-auto p-6 min-h-0">
           {children}
         </main>
       </div>
